72bae98b1d60dc3efd63c3fc9377714d35f9167e
Your Name <dutkamon.wo@gmail.com>
2026-06-08 20:52:59 +0200

    Change definition of % Reduction

--- 2026_project/main.py
+++ 2026_project/main.py
@@ -61,8 +61,8 @@ db_path = script_dir / "database.duckdb"
 ############################## [!!IMPORTANT!!]  IDENTIFY WHICH STEPS TO RUN ########################################################################################################
 
 # Change to 'True' if you need to (re)run OR 'False' to skip the step
-initialize = True # Step 1) initialize the adopt-net0 template [!!IMPORTANT!!] This study has added 2025 PPI data into "producer_price_index_euro.csv" file
-raw_prep = True # Step 2) ETL raw data to database
+initialize = False # Step 1) initialize the adopt-net0 template [!!IMPORTANT!!] This study has added 2025 PPI data into "producer_price_index_euro.csv" file
+raw_prep = False # Step 2) ETL raw data to database
 data_process = True # Step 3) data processing (create matrix, update Topology.json, prepare technology and network data)
 
 manual_update_network = True  # Optional step (if there is a manual update on node selection and transportation routes))
@@ -225,8 +225,8 @@ if prepare_inputs:
     # emission_limit: annual total emission × fraction of year modelled × target reduction (e.g. 0.2 = 80% reduction)
     # fraction_of_year_modelled is auto-derived from Topology.json start_date/end_date above.
     annual_total_emission = 66998708.72   # tCO2/year from all selected emitters
-    ccs_reduction_target  = 0.2           # 1.0 = no constraint, 0.2 = allow only 20% of baseline (80% reduction)
-    configuration["optimization"]["emission_limit"]["value"] = annual_total_emission * fraction_of_year_modelled * ccs_reduction_target
+    ccs_reduction_target  = 0.5           # % reduction
+    configuration["optimization"]["emission_limit"]["value"] = annual_total_emission * fraction_of_year_modelled * (1-ccs_reduction_target)
     #configuration["optimization"]["objective"]["value"] = "costs"
 
     # Set value to define MIP gap for the optimization solver
