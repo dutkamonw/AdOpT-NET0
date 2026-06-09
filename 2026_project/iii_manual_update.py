0e9d98867ef58688276528db3a55467b6a6f33da
Your Name <dutkamon.wo@gmail.com>
2026-06-10 01:35:57 +0200

    Fix calling gamma_matrix_v2 to v3

--- 2026_project/iii_manual_update.py
+++ 2026_project/iii_manual_update.py
@@ -12,7 +12,7 @@ import json
 import pandas as pd
 import duckdb
 from pathlib import Path
-from user_defined_function import canonicalize_name, create_gamma_matrix_v2, create_matrix
+from user_defined_function import canonicalize_name, create_gamma_matrix_v3, create_matrix
 
 ##################################################################################################
 
@@ -125,7 +125,7 @@ def manual_update():
 
 
     # ---- Create gamma matrix for Ship ----
-    create_gamma_matrix_v2(
+    create_gamma_matrix_v3(
         cost_model_type    = "ship",
         table_name         ="combined_selected_final",  # new table (to get emission data)
         distance_matrix    = output_path_data_processed / "network_topology_prep" / "CO2Ship" / "distance.csv",
@@ -159,7 +159,7 @@ def manual_update():
 
    
     # ---- Create gamma matrix for pipeline ----
-    create_gamma_matrix_v2(
+    create_gamma_matrix_v3(
         cost_model_type    = "pipeline",
         table_name         ="combined_selected_final",  # new table (to get emission data)
         distance_matrix    = output_path_data_processed / "network_topology_prep" / "CO2_Pipeline" / "distance.csv",
